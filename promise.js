9a225ebadad72a77fdcf2753c863c6d6af7ecdf9
muhammadniam <zeronejapanniam3@gmail.com>
2022-06-12 01:22:52 +0700

    feat: update promise in file promise.js

--- promise.js
+++ promise.js
@@ -1,8 +1,17 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (emosi) => {
+	try {
+		let bacaData1 = await promiseTheaterIXX();
+		let bacaData2 = await promiseTheaterVGC();
+		let gabung = bacaData1.concat(bacaData2);
+    let result = gabung.filter(el => el.hasil === emosi);
+    return result.length;
+	} catch (err) {
+		console.log(err);
+	}
+};
 module.exports = {
-  promiseOutput,
+	promiseOutput,
 };
